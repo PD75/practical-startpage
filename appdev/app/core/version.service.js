9c8e78aceb7ca6f71a1b590defd266408cb461fb
Paul <tigercub.75@gmail.com>
2016-02-19 09:59:31 +0100

    Manage data between upgrades

---
+++ appdev/app/core/version.service.js
@@ -0,0 +1,36 @@
+(function() {
+  'use strict';
+  angular.module('ps.core')
+    .factory('versionService', versionService);
+
+  function versionService(dataService) {
+    return {
+      checkVersion: checkVersion,
+    };
+
+    function checkVersion(newVersion, oldVersion) {
+
+
+      //from v1.x.x
+      if (angular.isUndefined(oldVersion) || oldVersion.charAt(0) < '2') {
+        if (angular.isDefined(dataService.data.bookmarkid)) {
+          var data = {};
+          data.quicklinks = [dataService.data.bookmarkid];
+          dataService.setData(data);
+        }
+        oldVersion = '2.0.0';
+      }
+      //from v2.0.0
+      if (oldVersion === '2.0.0') {
+        dataService.clearData('layout');
+      }
+
+      dataService.setData({
+        'version': newVersion,
+      });
+    }
+
+  }
+
+
+})();
